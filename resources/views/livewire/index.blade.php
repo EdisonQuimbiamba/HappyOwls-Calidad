2b481d99da52cf5e521e51708f0fd03b1b672c2a
Andrés <bryan.palma02@epn.edu.ec>
2021-07-28 15:09:52 -0500

    Se cambio el método para recuperar la imagen del directorio

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -1,6 +1,6 @@
 <div class="container py-8 ">
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         @foreach ($events as $event)
             <div class="container mx-auto px-4 d-flex align-items-stretch">
                 <div class="w-full max-w-md mx-auto bg-white shadow-md rounded-md px-6 py-4 my-6">
@@ -22,13 +22,13 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col col-lg-8 pt-2">
+                        <div class="col col-lg-6 pt-2">
                             <p class="text-gray-400 text-sm text-break">{{ Str::limit($event->body, 150) }}</p>
                         </div>
-                        <div class="col col-lg-4 image-wrapper flex justify-center">
+                        <div class="col col-lg-6 image-wrapper flex justify-center">
                             @if ($event->image)
                                 <img class="w-full h-full"
-                                    src="{{ Storage::url($event->image->url) }}" alt="{{$event->title}}">
+                                    src="{{ asset("storage/".$event->image->url) }}" alt="{{$event->title}}">
                             @else
                                 <img class="w-full h-full"
                                     src="https://picsum.photos/300/300" alt="">
